66f982703c6df77478a732b8691b6530cd929db4
PushkarK <krish1234pk@gmail.com>
2025-05-24 18:28:47 +0530

    Deleted files

--- convert_model.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-# Load the model (even if it was saved with TF 2.15+)
-model = tf.keras.models.load_model("model.h5")  # or model.keras
-
-# Re-save with TF 2.13 to downgrade serialization
-model.save("model_tf13.keras", save_format="keras")
